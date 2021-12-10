ea7d840538ca8b7c742b8cc40a521a14eefc3770
panditprogrammer <panditprogrammer@gmail.com>
2021-12-10 14:19:42 +0530

    while loop in ruby

---
+++ while_loop.rb
@@ -0,0 +1,19 @@
+# repeat using while loop 
+
+
+#print 5 times 
+i =0
+while i < 5
+    puts("Hello World")
+    i = i +1
+end
+
+
+# print number upto 10
+
+i= 1 # initialization
+while i < 11  # condition 
+    puts i   
+    i = i + 1   # increment
+    
+end
