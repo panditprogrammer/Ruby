91d3949df18c366d9246032f4124a6d7e57b7010
panditprogrammer <panditprogrammer@gmail.com>
2021-12-10 14:16:41 +0530

    variables and data types

---
+++ variables.rb
@@ -0,0 +1,16 @@
+# create varibles
+
+# string
+name = "ruby"
+prof = "developer"
+# integer 
+age = 22
+# boolean 
+isStudent = false
+# no value 
+address = nil
+# printing all variables value
+puts(name,age,isStudent,address)
+
+# print the variables with string 
+print("name is ",name, " and Age is ",age," prof is ",prof)
