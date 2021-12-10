a80bea2f3e4145f89dbc1a763837dfea39896292
panditprogrammer <panditprogrammer@gmail.com>
2021-12-10 14:32:59 +0530

    write comments in ruby

---
+++ comments_.rb
@@ -0,0 +1,22 @@
+# this is comments starts with '#' and this is one line comment 
+
+# this will show output
+puts ("hello ruby")
+
+# puts ("hello ruby")
+
+# create multiline comments
+
+
+=begin
+
+this kind of comments 
+ 1. start with '=begin'
+
+this is multiline comments
+this is for writing more description for another developer to understand purpose of written code 
+    
+2.  ends with '=end'
+
+
+=end
