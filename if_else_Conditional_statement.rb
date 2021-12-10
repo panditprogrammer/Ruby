83fa3e4673f28ff1cf402cd78283d13a6f6941ac
panditprogrammer <panditprogrammer@gmail.com>
2021-12-10 14:18:26 +0530

    if else conditional statement

---
+++ if_else_Conditional_statement.rb
@@ -0,0 +1,26 @@
+# checking condition using 'if' 'else' conditional statement 
+# a = 10
+# if a > 0
+#     puts "number is positive "
+# end
+    
+    
+# # if with else 
+# if a > 0
+#     puts "number is positive "
+# else
+#     puts "number is Negative "
+# end
+
+
+# multiple if else statement 
+x = 0
+if x > 0
+    puts "number is positive "
+elsif x < 0
+    puts "number is not positive "
+elsif x == 0
+    puts "number is Zero "
+else
+    puts "this is another " 
+end
