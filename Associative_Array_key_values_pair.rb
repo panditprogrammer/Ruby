385feada45fbdf422c7f3d1f3fe7f732d2717f24
panditprogrammer <panditprogrammer@gmail.com>
2021-12-10 14:20:53 +0530

    associative array in ruby key-value pair

---
+++ Associative_Array_key_values_pair.rb
@@ -0,0 +1,13 @@
+# create key and value pair using Hash or Associative array 
+course = {
+    "python" => 4500,
+    # :python => 4500,    same as above
+    "ruby" => 3300,
+    "java" => 2300,
+    "php" => 3000
+}
+
+# puts course[:python]
+puts course["python"]
+puts course["ruby"]
+
