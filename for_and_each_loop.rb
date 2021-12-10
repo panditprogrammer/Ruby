2bbcaf9b08e52844bae1eb21df5f030703ba39ce
panditprogrammer <panditprogrammer@gmail.com>
2021-12-10 14:20:11 +0530

    for and each and times toop

---
+++ for_and_each_loop.rb
@@ -0,0 +1,30 @@
+# for loop is used to iterate some iterable objects 
+
+# this is list
+numbers = [12,38,83,45,72,98,62]
+
+# # access all elements from numbers object 
+# for value in numbers
+#     puts value
+# end
+
+
+# # array 
+# languages = Array["python","ruby","java","CPP"]
+# # for each loop for iteration 
+# languages.each do |value|
+#     puts value
+# end
+
+
+# # printing value using some range
+# for i in 0..10
+#     puts i
+# end
+
+
+# repeat N times uisng   
+5.times do |x|
+    puts x
+end
+
