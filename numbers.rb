5dd82689b7dc942fa9739a69939dc63f3294bf6a
panditprogrammer <panditprogrammer@gmail.com>
2021-12-10 14:21:26 +0530

    number data types and all operations

---
+++ numbers.rb
@@ -0,0 +1,35 @@
+# integer data types and operations
+num = 43.45
+# puts num.round()
+
+# puts 34 + 7     #  41
+# puts 34 - 7     #  27
+# puts 34 * 7     #  238
+
+# #float  or decimal value
+# puts (34).to_f  / (7).to_f    #  4.857142857142857
+
+# puts 34 % 7     #  6
+# puts 4 ** 2     #   16
+
+# ============converting numbers========
+
+#int to float
+number = 45
+puts number.to_f 
+
+#float to int
+number = 34.23
+puts number.to_i
+
+# int to string 
+number = 20
+puts "this is string " + number.to_s  
+
+# string to int 
+number = "34"
+puts number.to_i
+
+# string to float 
+number = 45.23
+puts number.to_f
